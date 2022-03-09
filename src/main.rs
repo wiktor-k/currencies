17a50327750d9c2edc069a67860ae3e4377c7218
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2022-03-09 11:00:19 +0100

    Add version 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use chrono::Utc;
-use http::{Request, Response};
-use hyper::{
-    service::{make_service_fn, service_fn},
-    Body, Client, Server,
+use actix_web::{
+    middleware::Logger,
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
 };
-use hyper_tls::HttpsConnector;
-use std::{convert::Infallible, env, net::SocketAddr, sync::Arc};
-use uuid::Uuid;
+use awc::{
+    error::{JsonPayloadError, SendRequestError},
+    Client,
+};
 
 use serde::{Deserialize, Serialize};
 
@@ -40,120 +40,113 @@ struct Frame<'a> {
     icon: &'a str,
 }
 
-struct Handler<T: hyper::client::connect::Connect + Clone + Send + Sync + 'static> {
-    url: String,
-    client: hyper::client::Client<T>,
+fn render_to_frames(tables: Vec<Table>) -> Vec<Frame<'static>> {
+    let rates = tables.into_iter().flat_map(|table| table.rates);
+    let mut frames = vec![];
+
+    for rate in rates {
+        for currency in CURRENCIES {
+            if rate.code == currency.0 {
+                frames.push(Frame {
+                    text: format!("{:.4}", rate.mid),
+                    icon: currency.1,
+                });
+            }
+        }
+    }
+
+    frames
 }
 
-impl<T: hyper::client::connect::Connect + Clone + Send + Sync + 'static> Handler<T> {
-    fn create(url: String, connector: T) -> Self {
-        Self {
-            url,
-            client: Client::builder().build::<_, hyper::Body>(connector),
-        }
+#[derive(Debug)]
+struct Error;
+
+impl actix_web::error::ResponseError for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
     }
+}
 
-    async fn handle(&self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-        let request_id = Uuid::new_v4();
-        println!(
-            "ts={} id={} req={:?}",
-            Utc::now().to_rfc3339(),
-            request_id,
-            req
-        );
-
-        let req = Request::get(&self.url)
-            .header(
-                "User-Agent",
-                "currencies (+https://metacode.biz/@wiktor#currencies)",
-            )
-            .header("Accept", "application/json")
-            .body(hyper::body::Body::default())
-            .unwrap();
-
-        match self.client.request(req).await {
-            Ok(resp) => {
-                let status = resp.status().as_u16();
-                if status == 200 {
-                    let bytes = hyper::body::to_bytes(resp.into_body()).await?;
-                    let tables: Vec<Table> = serde_json::from_slice(&bytes).unwrap();
-                    let mut frames = Vec::new();
-                    for table in tables {
-                        for rate in table.rates {
-                            for currency in CURRENCIES {
-                                if rate.code == currency.0 {
-                                    frames.push(Frame {
-                                        text: format!("{:.4}", rate.mid),
-                                        icon: currency.1,
-                                    });
-                                }
-                            }
-                        }
-                    }
-                    let mut resp = Response::new(Body::from(
-                        serde_json::to_string_pretty(&Animation { frames }).unwrap(),
-                    ));
-                    let headers = resp.headers_mut();
-                    headers.append("Cache-Control", "max-age=3600".parse().unwrap());
-                    headers.append("Content-Type", "application/json".parse().unwrap());
-                    Ok(resp)
-                } else {
-                    println!(
-                        "ts={} id={} st={:?}",
-                        Utc::now().to_rfc3339(),
-                        request_id,
-                        status
-                    );
-
-                    Ok(Response::new(Body::from("error")))
-                }
-            }
-            Err(error) => {
-                println!(
-                    "ts={} id={} err={:?}",
-                    Utc::now().to_rfc3339(),
-                    request_id,
-                    error
-                );
-
-                Ok(Response::new(Body::from("error")))
-            }
-        }
+impl From<SendRequestError> for Error {
+    fn from(_: SendRequestError) -> Self {
+        Self
     }
+}
+
+impl From<JsonPayloadError> for Error {
+    fn from(_: JsonPayloadError) -> Self {
+        Self
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(_: serde_json::Error) -> Self {
+        Self
+    }
+}
+
+async fn render(client: Data<Client>) -> Result<HttpResponse, Error> {
+    let tables = client
+        .get("https://api.nbp.pl/api/exchangerates/tables/A/")
+        .insert_header((
+            "User-Agent",
+            "currencies (+https://metacode.biz/@wiktor#currencies2)",
+        ))
+        .insert_header(("Accept", "application/json"))
+        .send()
+        .await?
+        .json::<Vec<Table>>()
+        .await?;
+
+    let frames = render_to_frames(tables);
+
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .append_header(("Cache-Control", "max-age=3600"))
+        .append_header(("X-Collation-Id", uuid::Uuid::new_v4().to_string()))
+        .body(serde_json::to_string(&Animation { frames })?))
+}
+
+async fn healthz() -> impl Responder {
+    "OK"
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("ts={} status=starting", Utc::now().to_rfc3339());
-    let port = env::var("PORT")
-        .unwrap_or(String::from("3000"))
-        .parse()
-        .expect("PORT must be a number");
-    let url = env::var("URL").expect("Need target URL");
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    println!(
-        "ts={} status=listening addr={}",
-        Utc::now().to_rfc3339(),
-        addr
-    );
-
-    let handler = Arc::new(Handler::create(url, HttpsConnector::new()));
-
-    let make_svc = make_service_fn(|_| {
-        let handler = Arc::clone(&handler);
-        async move {
-            Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
-                let handler = Arc::clone(&handler);
-                async move { handler.handle(req).await }
-            }))
-        }
-    });
-
-    let server = Server::bind(&addr).serve(make_svc);
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    HttpServer::new(|| {
+        App::new()
+            .app_data(Data::new(Client::default()))
+            .route("/healthz", web::get().to(healthz))
+            .route("/", web::get().to(render))
+            .wrap(Logger::default())
+    })
+    .bind(std::env::var("BIND").unwrap_or_else(|_| "127.0.0.1:8080".into()))?
+    .run()
+    .await?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_fransform() {
+        let tables = vec![Table {
+            rates: vec![Rate {
+                code: "USD".into(),
+                mid: 1.23,
+            }],
+        }];
+        let frames = render_to_frames(tables);
+        assert_eq!(frames.len(), 1);
+        let frame = &frames[0];
+        assert_eq!(frame.text, "1.2300");
+        assert_eq!(frame.icon, "i4935");
+    }
 }
