31bc9bc127ddf4a95915387f8cb9e932b4fc16ea
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2022-05-23 11:25:57 +0200

    Add healthz test
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

---
+++ tests/health.rs
@@ -0,0 +1,15 @@
+use currencies::start;
+use std::net::TcpListener;
+
+#[actix_rt::test]
+async fn healthz_test() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let address = format!("http://127.0.0.1:{}/healthz", listener.local_addr()?.port());
+    let _ = tokio::spawn(start(listener.into())?);
+    let client = awc::Client::default();
+    let response = client.get(address).send().await.unwrap();
+
+    assert_eq!(200, response.status().as_u16());
+
+    Ok(())
+}
